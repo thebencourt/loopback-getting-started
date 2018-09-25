84a99ed9137a59c3a85cd84da2a8e454d5ea8e47
Ben Court <thebencourt@gmail.com>
2018-09-25 16:20:24 +0100

    Add remote method

--- common/models/coffee-shop.js
+++ common/models/coffee-shop.js
@@ -1,5 +1,27 @@
 'use strict';
 
-module.exports = function(Coffeeshop) {
+module.exports = (CoffeeShop) => {
+  CoffeeShop.status = (cb) => {
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+    const OPEN_HOUR = 6;
+    const CLOSE_HOUR = 20;
+    const isOpen = currentHour >= OPEN_HOUR && currentHour < CLOSE_HOUR;
+    const openMsg = 'We are open for business.'
+    const closedMsg = `Sorry, we are closed. Open daily from ${OPEN_HOUR}am to ${CLOSE_HOUR}pm.`;
+    cb(null, isOpen ? openMsg : closedMsg);
+  };
 
+  CoffeeShop.remoteMethod(
+    'status', {
+      http: {
+        path: '/status',
+        verb: 'get'
+      },
+      returns: {
+        arg: 'status',
+        type: 'string'
+      }
+    }
+  );
 };
