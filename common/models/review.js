241104b5eb22f44bd5b5ff4e52af1af4ba8c0288
Ben Court <thebencourt@gmail.com>
2018-09-26 09:19:40 +0100

    Add remote hook

--- common/models/review.js
+++ common/models/review.js
@@ -1,5 +1,9 @@
 'use strict';
 
-module.exports = function(Review) {
-
+module.exports = (Review) => {
+  Review.beforeRemote('create', ({ args, req }, user, next) => {
+    args.data.date = Date.now();
+    args.data.publisherId = req.accessToken.userId;
+    next();
+  });
 };
